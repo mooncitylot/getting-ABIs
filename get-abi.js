79c6aafed98c163373be031f2fb4ffe9dccffefd
Tyler <mooncitylot@gmail.com>
2024-01-30 10:53:09 -0600

    Update address for FreshDAO verification

--- get-abi.js
+++ get-abi.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const ethers = require("ethers");
 
 //TODO: Figure out how to get the address for FreshDAO verified... if possible....
-const address = "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619";
+const address = "0x9CB56f993260790Bb91a59104e75cb084Bc374e0";
 
 const apiKey = "X8BUYYT47EW9NK48KG6V7AHU8SG9SQPFB3";
 
@@ -14,8 +14,12 @@ const url = `https://api.polygonscan.com/api
 
 const getabi = async () => {
   const res = await axios.get(url);
-  const abi = JSON.parse(res.data.result);
-  console.log(abi);
+  try {
+    const abi = JSON.parse(res.data.result);
+    console.log(abi);
+  } catch (error) {
+    console.error("Error parsing JSON:", res.data.result);
+  }
 };
 
 getabi();
